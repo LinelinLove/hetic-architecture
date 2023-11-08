daea990b06695276ecac84d4de3db25f72c047be
Jyaax <julie.lcny@gmail.com>
2023-11-08 15:03:37 +0100

    Ajout de valeurs dans content pour prévenir l erreur tailwind dans tailwind.config.js

--- architecture-anime-manga/tailwind.config.js
+++ architecture-anime-manga/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./app/**/*.{html,js,ts,jsx,tsx}",
+    "./resources/views/**/*.blade.php",
+    // Ajoute d'autres fichiers ou répertoires que tu souhaites inclure ici
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};

1ba4f36160696a6cd1b98edbe7ed6baf3b3c8f54
Linelinlove <chrisline.lin@outlook.fr>
2023-11-19 23:32:42 +0100

    login logout

--- architecture-anime-manga/src/components/Header.jsx
+++ architecture-anime-manga/src/components/Header.jsx
@@ -1,8 +1,32 @@
 // Header.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { app } from "../firebaseConfig";
+const auth = getAuth(app);
 
 export default function Header() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsUserLoggedIn(!!user); // Met à jour l'état en fonction de la présence de l'utilisateur
+    });
+
+    // Nettoyez l'écouteur lorsque le composant est démonté
+    return () => unsubscribe();
+  }, []); // Assurez-vous de déclencher cet effet une seule fois lors du montage
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("User signed out successfully");
+      // Vous pouvez rediriger l'utilisateur vers une page de connexion ou effectuer d'autres actions après la déconnexion.
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
   return (
     <header>
       <nav className="flex flex-row place-content-between bg-blue-100 text-black p-4">
@@ -23,17 +47,16 @@ export default function Header() {
             <Link to="/settings">Paramètres</Link>
           </li>
           <li>
-            <Link to="/login">Se connecter</Link>
-          </li>
-          <li>
             <Link to="/signup">S'inscrire</Link>
           </li>
           <li>
             <Link to="/contact">Contact</Link>
           </li>
-          <li>
-            <Link to="/logout">Déconnexion</Link>
-          </li>
+          {isUserLoggedIn ? (
+            <Link onClick={handleSignOut}>Se déconnecter</Link>
+          ) : (
+            <Link to="/login">Se connecter</Link>
+          )}
         </ul>
       </nav>
     </header>

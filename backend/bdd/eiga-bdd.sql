6b9a13f3c036fca5b192ac3be0250ba789dc3879
Linelinlove <chrisline.lin@outlook.fr>
2023-11-28 22:28:19 +0100

    bdd + delete useless files

---
+++ backend/bdd/eiga-bdd.sql
@@ -0,0 +1,49 @@
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `firstname` varchar(255),
+  `lasname` varchar(255),
+  `username` varchar(255) UNIQUE;
+  `mail` varchar(255),
+  `telephone` int,
+  `pwd` varchar(255),
+  `gender` varchar(255),
+  `uid_firebase` int,
+  `profil_picture` blob,
+  `birthday` date,
+  `role` varchar(255)
+);
+
+CREATE TABLE `watchlist` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `anime_id` int,
+  `status` varchar(255),
+  `current_episode` int
+);
+
+CREATE TABLE `list_favorite` (
+  `user_id` int,
+  `anime_id` int
+);
+
+CREATE TABLE `note` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `anime_id` int,
+  `note` int
+);
+
+CREATE TABLE `commentaire` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `anime_id` int,
+  `commentaire` varchar(255)
+);
+
+ALTER TABLE `watchlist` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `list_favorite` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `note` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `commentaire` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
